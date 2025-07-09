aa4c733174253d7048ca363404b7f59c7086ad52
Jhoel Sequeira <56564490+Jhoel-Sequeira@users.noreply.github.com>
2025-07-08 20:28:44 -0600

    Veterinario

--- controllers/sistema.py
+++ controllers/sistema.py
@@ -1828,7 +1828,7 @@ def tablaConsultas():
 
         conn = conectar()
         cursor = conn.cursor()
-        query = "select a.cod_atencion,a.fecha_atencion,c.nombres_cliente + ' ' + c.apellidos_cliente as Nombre,e.NombreEstado,m.Nombre_mascota,es.nom_especie,ta.tipo,a.peso,a.altura,a.temperatura from atencion as a inner join cliente as c on a.num_cliente = c.num_cliente inner join estado as e on a.id_estado = e.id_estado inner join mascota as m on a.idMascota = m.idMascota inner join tipo_atencion as ta on a.tipo_atencion = ta.cod_tipo inner join raza as r on m.id_raza = r.id_raza inner join especie as es on r.id_especie = es.id_especie order by a.cod_atencion ASC"
+        query = "select a.cod_atencion,a.fecha_atencion,c.nombres_cliente + ' ' + c.apellidos_cliente as Nombre,e.NombreEstado,m.Nombre_mascota,es.nom_especie,ta.tipo,a.peso,a.altura,a.temperatura,vet.nombres_cliente as veterinario from atencion as a inner join cliente as c on a.num_cliente = c.num_cliente inner join estado as e on a.id_estado = e.id_estado inner join mascota as m on a.idMascota = m.idMascota inner join tipo_atencion as ta on a.tipo_atencion = ta.cod_tipo inner join raza as r on m.id_raza = r.id_raza inner join especie as es on r.id_especie = es.id_especie inner join cliente as vet on a.num_veterinario = vet.num_cliente order by a.cod_atencion DESC"
         cursor.execute(query)
         consultas = cursor.fetchall()
 
@@ -1948,7 +1948,7 @@ def tablaConsultasProductos():
         
         conn = conectar()
         cursor = conn.cursor()
-        query = "select a.cod_detalle,p.nom_producto,p.precio,a.cantidad,a.orientacion from atencion_producto as a inner join producto as p on a.cod_producto = p.cod_producto where a.cod_atencion = ?"
+        query = "select a.cod_detalle,p.nom_producto,p.precio,a.cantidad,a.orientacion from atencion_producto as a inner join producto as p on a.cod_producto = p.cod_producto where a.cod_atencion = ? order by cod_atencion DESC"
         cursor.execute(query,(atencion))
         consultas = cursor.fetchall()
 
